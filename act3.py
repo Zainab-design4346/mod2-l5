23e527aeb8a7813ec1bccd4fc9f2b691db284f51
Zainab-design4346 <zainabshahzad.4346@gmail.com>
2026-05-10 15:55:57 +0400

    Add files via upload

---
+++ act3.py
@@ -0,0 +1,8 @@
+import math
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+result = math.lcm(num1, num2)
+print(f"The lcm of {num1} and {num2} is {result}")
+
+
